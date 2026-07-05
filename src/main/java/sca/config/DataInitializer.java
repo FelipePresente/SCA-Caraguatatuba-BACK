f56a250a1ec372fb55950fd65a06bc308d18b4ce
Felipe Presente <felipe.presente@outlook.com>
2026-07-05 01:45:57 -0300

    comidas default adicionadas

--- src/main/java/sca/config/DataInitializer.java
+++ src/main/java/sca/config/DataInitializer.java
@@ -4,6 +4,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import sca.food.Food;
+import sca.food.FoodRepository;
 import sca.role.Role;
 import sca.role.RoleRepository;
 import sca.user.User;
@@ -13,24 +15,35 @@ import sca.user.UserRepository;
 public class DataInitializer implements CommandLineRunner {
     private final RoleRepository roleRepository;
     private final UserRepository userRepository;
+    private final FoodRepository foodRepository;
     private final PasswordEncoder passwordEncoder;
 
     public DataInitializer(RoleRepository roleRepository, UserRepository userRepository,
-            PasswordEncoder passwordEncoder) {
+            PasswordEncoder passwordEncoder, FoodRepository foodRepository) {
         this.roleRepository = roleRepository;
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.foodRepository = foodRepository;
     }
 
     @Override
     public void run(String... args) {
-        if (roleRepository.count() == 0) {
-            roleRepository.save(new Role("school"));
-            roleRepository.save(new Role("seduc_user"));
-            roleRepository.save(new Role("admin"));
-            System.out.println("Cargos 'school', 'seduc_user' e 'admin' criados");
-        }
+        createRoles();
+        createAdminUser();
+        createDefaultFood();
+    }
 
+    public void createRoles() {
+        String[] roleNames = { "school", "seduc_user", "admin" };
+        for (String roleName : roleNames) {
+            if (roleRepository.findByName(roleName).isEmpty()) {
+                roleRepository.save(new Role(roleName));
+                System.out.println("Cargo " + roleName + " criado");
+            }
+        }
+    }
+
+    public void createAdminUser() {
         if (roleRepository.findByName("admin").isPresent()) {
             Role adminRole = roleRepository.findByName("admin")
                     .orElseThrow(() -> new RuntimeException("Cargo 'admin' não encontrado"));
@@ -43,6 +56,28 @@ public class DataInitializer implements CommandLineRunner {
             }
 
             System.out.println("Usuário admin padrão criado");
+        }
+    }
+
+    // Essa função executa somente se nenhuma comida existir no banco
+    public void createDefaultFood() {
+        if (this.foodRepository.count() > 0) {
+            return;
+        }
+
+        String[] foodNames = {
+                "Feijão", "Arroz", "Macarrão", "Bife", "Frango", "Peixe", "Legumes", "Fruta",
+                "Suco", "Pão", "Bolo", "Coxinha", "Pastel", "Salgado", "Refrigerante",
+                "Água", "Café", "Chá", "Leite", "Iogurte"
+        };
+        Double[] foodPrices = {
+                5.77, 6.32, 7.37, 15.43, 12.72, 13.37, 6.17, 10.22,
+                8.47, 4.12, 11.07, 9.62, 10.22, 10.87, 13.37,
+                2.17, 6.17, 5.12, 5.12, 8.47
+        };
+        for (int i = 0; i < foodNames.length; i++) {
+            this.foodRepository.save(new Food(foodNames[i], foodPrices[i]));
+            System.out.println("Comida " + foodNames[i] + " criada");
         }
     }
 }
